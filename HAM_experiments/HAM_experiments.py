63c6c3fcab123e5cd4dd52ac106fcfb2920d120a
Tviskaron <tviskaron@gmail.com>
2017-12-21 21:52:40 +0300

    HAM experiment with state encoded ArmEnv

--- HAM_experiments/HAM_experiments.py
+++ HAM_experiments/HAM_experiments.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 import sys
+
+from HAM.machines_on_arm import BasicMachineArm
+from environments.arm_env import ArmEnv
 from environments.maze_world_env import MazeWorld, MazeWorldEpisodeLength
 from lib import plotting
 from HAM.utils import ham_learning
-from HAM.machines import *
+from HAM.machines_on_grid_maze import *
 from environments.grid_maze_generator import *
 import threading
 
@@ -433,5 +436,21 @@ def experiment_12():
                              )
 
 
+def experiment_13():
+    env = ArmEnv()
+
+    env.render()
+    params = {
+        "env": env,
+        "num_episodes": 550,
+        "machine": BasicMachineArm,
+        "alpha": 0.1,
+        "epsilon": 0.1,
+        "discount_factor": 0.9,
+    }
+    Q, stats = ham_learning(**params)
+    plotting.plot_multi_test(curve_to_draw=[stats.episode_rewards])
+
+
 if __name__ == "__main__":
-    experiment_12()
+    experiment_13()
