aa76a87debc3a2bd38631cec776adfd10358a06e
Tviskaron <tviskaron@gmail.com>
2017-12-25 17:07:36 +0300

    ArmEnv machines added

--- HAM/machines_on_arm.py
+++ HAM/machines_on_arm.py
@@ -49,3 +49,102 @@ class BasicMachineArm(HAM):
 
     def stop(self, info):
         pass
+
+
+class SmartMachine(BasicMachineArm):
+    def start(self, info):
+        return self.choice(info)
+
+    def choice(self, info):
+        choices = [self.action_right, self.action_on, self.action_off, self.action_left, self.action_off, self.call_smart]
+        name = info["prefix_machine"] + __name__ + ":" + self.who_a_mi()
+        choices[self.choice_update(info, choices, name)](info)
+
+    def call_smart(self, info):
+        self.call(info, Smart)
+        self.choice(info)
+
+
+class Smart(HAM):
+    def start(self, info):
+        return self.action_on(info)
+
+    def action_on(self, info):
+        action = ON
+        self.apply_action(info, action)
+        self.action_down_1(info)
+
+    def action_down_1(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_down_2(info)
+
+    def action_down_2(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_down_3(info)
+
+    def action_down_3(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_up_1(info)
+
+    def action_up_1(self, info):
+        action = UP
+        self.apply_action(info, action)
+        self.action_up_2(info)
+
+    def action_up_2(self, info):
+        action = UP
+        self.apply_action(info, action)
+        self.action_up_3(info)
+
+    def action_up_3(self, info):
+        action = UP
+        self.apply_action(info, action)
+        # end
+
+
+class SmartMachineTest(BasicMachineArm):
+    def start(self, info):
+        return self.choice(info)
+
+    def choice(self, info):
+        choices = [self.action_right, self.action_on, self.action_off, self.action_left, self.action_off, self.action_on]
+        name = info["prefix_machine"] + __name__ + ":" + self.__class__.__name__ + ":" + self.who_a_mi()
+        # print(name)
+        choices[self.choice_update(info, choices, name)](info)
+
+    def action_on(self, info):
+        action = ON
+        self.apply_action(info, action)
+        self.action_down_1(info)
+
+    def action_down_1(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_down_2(info)
+
+    def action_down_2(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_down_3(info)
+
+    def action_down_3(self, info):
+        action = DOWN
+        self.apply_action(info, action)
+        self.action_up_1(info)
+
+    def action_up_1(self, info):
+        action = UP
+        self.apply_action(info, action)
+        self.action_up_2(info)
+
+    def action_up_2(self, info):
+        action = UP
+        self.apply_action(info, action)
+        self.action_up_3(info)
+
+    def action_up_3(self, info):
+        action = UP
+        self.apply_action(info, action)
