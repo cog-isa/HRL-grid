2eec4b85138db850465f10c3dd70c14d358bc481
Tviskaron <tviskaron@gmail.com>
2018-01-23 19:13:36 +0300

    refactoring

---
+++ environments/arm_env/console_interact.py
@@ -0,0 +1,28 @@
+from environments.arm_env.arm_env import ArmEnv
+
+if __name__ == "__main__":
+
+    print("\n" * 100)
+
+    c_env = ArmEnv(size_x=3, size_y=3, cubes_cnt=3, episode_max_length=100, finish_reward=100, action_minus_reward=-1, tower_target_size=3)
+    _, rew, is_done = (None, None, None)
+    for i in range(100):
+        print('\n' * 100)
+        c_env.render()
+
+        print(rew, is_done)
+
+        print("0 LEFT")
+        print("1 UP")
+        print("2 RIGHT")
+        print("3 DOWN")
+        print("4 ON")
+        print("5 OFF")
+
+        while True:
+            try:
+                act = int(input())
+                break
+            except ValueError:
+                pass
+        _, reward, done, _ = c_env.step(act)
