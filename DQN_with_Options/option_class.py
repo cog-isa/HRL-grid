6022c4f46499e98c386e5ef705dbd5c633d51bfa
vtkuzmin <vtkuzmin@edu.hse.ru>
2018-05-14 23:31:17 +0300

    DQN with Options

---
+++ DQN_with_Options/option_class.py
@@ -0,0 +1,58 @@
+import sys
+import gym.spaces
+import itertools
+import numpy as np
+import random
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+from collections import namedtuple
+from dqn_utils import *
+
+class option(object):
+    def __init__(self, env, stop_condition, path, import_scope, name = None, max_t = 20):
+        
+        self.max_t = max_t
+        self.g = tf.Graph()
+        self.sess = tf.Session(graph=self.g)
+        self.stop_condition = stop_condition
+
+        with self.g.as_default():
+            self.saver = tf.train.import_meta_graph(path+".meta", import_scope = import_scope)
+            self.saver.restore(self.sess, path)
+        
+        self.obs_t_float = self.g.get_tensor_by_name(import_scope+"/obs_t_float:0")
+        self.pred_ac = self.g.get_tensor_by_name(import_scope+"/pred_ac:0")
+    
+    def encode_observation(self, frame):
+        img_h, img_w = frame.shape[1], frame.shape[2]
+        return frame.transpose(1, 2, 0, 3).reshape(img_h, img_w, -1)
+        
+    
+    def step(self, env):
+        """Executes the option"""
+        opt_reward = 0.0
+        opt_length = 0.0
+        last_obs = env.get_evidence_for_image_render()
+#         env.render()
+        
+        for t in itertools.count():
+            obs = self.encode_observation(np.array([last_obs]))
+            action = self.sess.run(self.pred_ac, {self.obs_t_float: [obs]})[0]
+
+            next_obs, reward, done, info = env.step(action)
+
+            opt_reward += reward
+            opt_length += 1
+            
+#             env.render()
+
+            if done or self.stop_condition(env) or t >= self.max_t:
+                break
+
+            last_obs = next_obs
+            
+        return next_obs, opt_reward, done, opt_length, info
+        
+        
+    
+
---
+++ DQN_with_Options/plotting.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from collections import namedtuple
+from matplotlib import pyplot as plt
+
+EpisodeStats = namedtuple("Stats", ["episode_lengths", "episode_rewards"])
+
+
+def plot_episode_stats(stats, smoothing_window=10, no_show=False):
+    # Plot the episode length over time
+    fig1 = plt.figure(figsize=(10, 5))
+    plt.plot(stats.episode_lengths)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Length")
+    plt.title("Episode Length over Time")
+    if no_show:
+        plt.close(fig1)
+    else:
+        plt.savefig("diagram_len_over_opt2_6_4.png")
+        plt.show(fig1)
+
+    # Plot the episode reward over time
+    fig2 = plt.figure(figsize=(10, 5))
+    rewards_smoothed = pd.Series(stats.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(rewards_smoothed)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Reward (Smoothed)")
+    plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
+    if no_show:
+        plt.close(fig2)
+    else:
+        plt.savefig("diagram_rew_over_opt2_6_4.png")
+        plt.show(fig2)
+
+
+def plot_multi_test(curve_to_draw=None, smoothing_window=10, x_label="X", y_label="Y", labels=None):
+    fig2 = plt.figure(figsize=(10, 5))
+
+    t = []
+    for index, elem in enumerate(curve_to_draw):
+        rewards_smoothed = pd.Series(elem).rolling(smoothing_window, min_periods=smoothing_window).mean()
+        p, = plt.plot(rewards_smoothed)
+        t.append(p)
+    plt.legend(t, labels) if labels else plt.legend(t)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.savefig("diagram.png")
+    plt.show(fig2)
